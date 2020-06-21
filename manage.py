0d475e6d329456f666bb573817497b9f9be8f494
Debasish Nandi <debasishfairytail@gmail.com>
2020-06-21 19:53:37 +0530

    commit 1

--- manage.py
+++ manage.py
@@ -12,5 +12,4 @@ if __name__ == "__main__":
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         )
-    print(os.listdir("D:/"))
     execute_from_command_line(sys.argv)
